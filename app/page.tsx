1caeba3de901d5be459eb5e0f76eb8c3b1fc56b1
tux <tux@tux.localdomain>
2023-08-23 18:45:58 -0300

    local data fix

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const Home = () => {
       setIsLoading(false);
     }
 
-    const coins = await request(
+    const coins: any = await request(
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=%221h%2C%2024h%2C%207d%22&locale=en'
     );
 
